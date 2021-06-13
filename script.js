fa03796d03d17fdc9b34c48dc2b69d94d106f4b5
Henry Parras <henry.parras@yahoo.com>
2021-06-12 19:47:19 -0700

    weather icon on top and 5 day loop stopped at 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var key = "034a6c6a724325798e1f8e5a33949fe6";
-
+//var momento = moment().format('dddd, MMMM Do YYYY');
 
 var cityNameEl = document.querySelector("#cityName");
 var buttonEl = document.querySelector("#submitButton");
@@ -9,7 +9,7 @@ var humidityEl = document.querySelector("#humidity");
 var windEl = document.querySelector("#wind");
 var uviEl = document.querySelector("#uvi");
 var weatherIconEl = document.querySelector("#weatherIcon");
-
+//var momentoEl = document.querySelector("#momento");
 
 buttonEl.addEventListener('click', getCityname);
 
@@ -86,7 +86,7 @@ function getCityname(event) {
                     windEl.innerHTML =  `Wind: ${weatherDetails.cityWind}`;      
                     uviEl.innerHTML =  `UVI: ${weatherDetails.cityUvi}`;
                     weatherIconEl.innerHTML = "<img src='https://openweathermap.org/img/w/"+ weatherDetails.cityWeatherIcon + ".png'>";
-               
+                    //momentoEl.innerHTML =  `${momento}`;
                 
                 
                 
@@ -111,12 +111,13 @@ function getCityname(event) {
                               console.log(data.daily[0].temp.day)  
                               
                                 for (var i = 0; i < data.daily.length; i++) {
+                                    if (i === 5)  break;
                                 var days = data.daily[i]
                             
-
+                                
                                 console.log(days);
                                 console.log(days.humidity);
-                            
+                                
                             }
                                 //var day= [{
                                     
